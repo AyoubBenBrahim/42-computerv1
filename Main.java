b2b2be3d7ff3861c79b28073f4fc463fa7935671
Ayoub Bouras <aybouras@student.1337.ma>
2022-04-25 13:23:31 +0000

    solving

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         String[] arr = input.split("=");
         String leftHandSide = arr[0];
         String rightHandSide = arr[1];
-        rightHandSide = "8*X^0-6*X^1+1*X^2-5.6*X^3+2222222-15";
+        rightHandSide = "8.2*X^10-6*X^4+11*X^2-5.6*X^3+2222222-15-8*X^2";
         if (rightHandSide.matches("^\\d.+")) {
             rightHandSide = ("+").concat(rightHandSide);
         }
@@ -24,7 +24,6 @@ public class Main {
             } else if (rightHandSide.charAt(i) == '-') {
                 rightHandSide = rightHandSide.substring(0, i) + "#+" + rightHandSide.substring(i + 1);
                 i++;
-
             }
         }
         System.out.println(rightHandSide);
@@ -37,8 +36,8 @@ public class Main {
         // String[] splitString = rightHandSide.split("\\*|[A-z]|\\^");
         String[] splitString = rightHandSide.split("#");
 
-        for (String s : splitString)
-            System.out.println(s);
+        // for (String s : splitString)
+        // System.out.println(s);
 
         System.out.println("len = " + splitString.length);
 
@@ -46,23 +45,93 @@ public class Main {
         for (String str : splitString) {
             // for (int i = 0; i < str.length(); i++) {
             // if (rightHandSide.charAt(i) == '+')
-            // System.out.println(str);
-            if (!str.isEmpty() && str.indexOf('*') == -1) {
-                sum += Float.parseFloat(str);
+            System.out.println("===== " + str + " ======");
+            if (str.indexOf('*') == -1) {
+                if (!str.isEmpty())
+                    sum += Float.parseFloat(str);
             } else {
+                // System.out.println("indexOf('*') = " + str.indexOf('*'));
+                // System.out.println("substring(0, str.indexOf('*') = " + str.substring(0,
+                // str.indexOf('*')));
+                // System.out.println("indexOf('^') = " + str.indexOf('^'));
+                // System.out.println("substring(str.indexOf('^') = " +
+                // str.substring(str.indexOf('^') + 1));
+
+                // System.out.println("coef = " + Float.parseFloat(str.substring(0,
+                // str.indexOf('*'))));
+                // System.out.println("ouus = " +
+                // Integer.parseInt(str.substring(str.indexOf('^') + 1)));
 
                 PolynomialEquation p = new PolynomialEquation(
                         Float.parseFloat(str.substring(0, str.indexOf('*'))),
-                        Integer.parseInt(str.substring(str.indexOf('^'))));
+                        Integer.parseInt(str.substring(str.indexOf('^') + 1)));
 
                 Polynomials.add(p);
             }
         }
 
         System.out.println(sum);
+        Polynomials.forEach(p -> System.out.println(p));
 
-        for (PolynomialEquation p : Polynomials)
-            System.err.println(p.toString());
+        Collections.sort(Polynomials, new Comparator<PolynomialEquation>() {
+            @Override
+            public int compare(PolynomialEquation p1, PolynomialEquation p2) {
+                return Integer.compare(p1.getDegree(), p2.getDegree());
+            }
+        });
+
+        System.out.println("****** sorted ****");
+
+        Polynomials.forEach(p -> System.out.println(p));
+
+        System.out.println("****** Sum ****");
+
+        // for (PolynomialEquation p : Polynomials) {
+
+        // }
+
+        ListIterator<PolynomialEquation> iterator = Polynomials.listIterator();
+
+        while (iterator.hasNext()) {
+            // if (Integer.compare(iterator.next().getDegree(), iterator.)
+            System.out.println(iterator.next());
+        }
+
+        ArrayList<Integer> numbs = new ArrayList<Integer>();
+        numbs.add(1);
+        numbs.add(1);
+        numbs.add(1);
+        numbs.add(2);
+        numbs.add(2);
+        numbs.forEach(p -> System.out.println(p));
+        System.out.println("*********");
+        // Get the iterator
+        // Iterator<String> it = numbs.iterator();
+
+        // while (it.hasNext())
+        // System.out.println((it.next()));
+
+        for (int i = 0; i < numbs.size() - 1; i++) {
+            // System.out.println(numbs.get(i) + " next " + numbs.get(i + 1));
+
+            if (numbs.get(i) == numbs.get(i+1) || (i >= 1 && numbs.get(i) == numbs.get(i - 1)))
+            {
+                int somme = numbs.get(i) + numbs.get(i+1);
+                numbs.set(i+1, somme * 10000);
+                numbs.remove(i);
+                // i--;
+                // numbs.add(i, somme * 10000);
+                // numbs.remove(i + 1);
+            }
+            // if (i == 2) {
+            //     numbs.remove(i);
+            //     numbs.add(i, i * 100);
+            // }
+
+        }
+
+        // numbs.add(0, 200);
+        numbs.forEach(p -> System.out.println(p));
 
     }
 
@@ -109,6 +178,19 @@ class PolynomialEquation {
         this.coefficient = cf;
         // this.variable = v;
         this.degree = d;
+    }
+
+    public float getCoefficient() {
+        return coefficient;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    @Override
+    public String toString() {
+        return this.coefficient + " ^ " + this.degree;
     }
 
 }
