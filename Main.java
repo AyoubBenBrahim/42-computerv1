f9ec617f11b79341d464f49155bc142ba9557f33
Ayoub Bouras <aybouras@student.1337.ma>
2022-04-25 18:26:11 +0000

    solving

--- Main.java
+++ Main.java
@@ -5,18 +5,22 @@ import java.util.regex.Pattern;
 
 public class Main {
     private static final String pattern = "^(?!\\*)((\\+|-)?(?!$|\\+|-|=|\\*)((\\d+(\\.\\d+)?)?(\\*(?=[A-z]))?([A-z](\\^\\d+)?)?)(?=\\+|-|=|$))+={1}((\\+|-)?(?!$|\\+|-|\\*)((\\d+(\\.\\d+)?)?(\\*(?=[A-z]))?([A-z](\\^\\d+)?)?)(?=\\+|-|$))+$";
+    private static Integer maxDegree;
+    public static ArrayList<PolynomialEquation> Polynomials = new ArrayList<PolynomialEquation>();
 
-    static void reducedForm(String input) {
-        ArrayList<PolynomialEquation> Polynomials = new ArrayList<PolynomialEquation>();
+    static String reducedForm(String input) {
 
         String[] arr = input.split("=");
         String leftHandSide = arr[0];
         String rightHandSide = arr[1];
-        rightHandSide = "8.2*X^10-6*X^4+11*X^2-5.6*X^3+2222222-15-8*X^2";
-        if (rightHandSide.matches("^\\d.+")) {
+        // rightHandSide = "8.123*X^109991-6*X^4+11*X^2-5.6*X^3+33-15-8*X^2";
+        // leftHandSide = "2*X^5-6*X^4+11*X^2-5.6*X^3+22-15-18*X^2";
+
+        if (rightHandSide.matches("^\\d.+"))
             rightHandSide = ("+").concat(rightHandSide);
-        }
-        // System.out.println(rightHandSide);
+        if (leftHandSide.matches("^\\d.+"))
+            leftHandSide = ("+").concat(leftHandSide);
+        System.out.println(rightHandSide);
         for (int i = 0; i < rightHandSide.length(); i++) {
             if (rightHandSide.charAt(i) == '+') {
                 rightHandSide = rightHandSide.substring(0, i) + "#-" + rightHandSide.substring(i + 1);
@@ -26,42 +30,25 @@ public class Main {
                 i++;
             }
         }
+        leftHandSide = leftHandSide.replace("+", "#+");
+        leftHandSide = leftHandSide.replace("-", "#-");
+
+        String fullEquation = leftHandSide.concat(rightHandSide);
         System.out.println(rightHandSide);
+        System.out.println("full = " + fullEquation);
 
-        // String tab[] = rightHandSide.split("#");
-        // for (String s : tab)
-        // System.out.println(s);
-
-        // String[] splitString = rightHandSide.split("\\*|[A-z]|\\#");
-        // String[] splitString = rightHandSide.split("\\*|[A-z]|\\^");
-        String[] splitString = rightHandSide.split("#");
-
-        // for (String s : splitString)
-        // System.out.println(s);
+        String[] splitString = fullEquation.split("#");
 
         System.out.println("len = " + splitString.length);
 
-        float sum = 0;
+        float sumNumbers = 0;
+
         for (String str : splitString) {
-            // for (int i = 0; i < str.length(); i++) {
-            // if (rightHandSide.charAt(i) == '+')
             System.out.println("===== " + str + " ======");
             if (str.indexOf('*') == -1) {
                 if (!str.isEmpty())
-                    sum += Float.parseFloat(str);
+                    sumNumbers += Float.parseFloat(str);
             } else {
-                // System.out.println("indexOf('*') = " + str.indexOf('*'));
-                // System.out.println("substring(0, str.indexOf('*') = " + str.substring(0,
-                // str.indexOf('*')));
-                // System.out.println("indexOf('^') = " + str.indexOf('^'));
-                // System.out.println("substring(str.indexOf('^') = " +
-                // str.substring(str.indexOf('^') + 1));
-
-                // System.out.println("coef = " + Float.parseFloat(str.substring(0,
-                // str.indexOf('*'))));
-                // System.out.println("ouus = " +
-                // Integer.parseInt(str.substring(str.indexOf('^') + 1)));
-
                 PolynomialEquation p = new PolynomialEquation(
                         Float.parseFloat(str.substring(0, str.indexOf('*'))),
                         Integer.parseInt(str.substring(str.indexOf('^') + 1)));
@@ -70,9 +57,8 @@ public class Main {
             }
         }
 
-        System.out.println(sum);
+        System.out.println("Summmmm " + sumNumbers);
         Polynomials.forEach(p -> System.out.println(p));
-
         Collections.sort(Polynomials, new Comparator<PolynomialEquation>() {
             @Override
             public int compare(PolynomialEquation p1, PolynomialEquation p2) {
@@ -86,49 +72,45 @@ public class Main {
 
         System.out.println("****** Sum ****");
 
-        // for (PolynomialEquation p : Polynomials) {
-
-        // }
-
-        // ListIterator<PolynomialEquation> iterator = Polynomials.listIterator();
-        // while (iterator.hasNext()) {
-        //     // if (Integer.compare(iterator.next().getDegree(), iterator.)
-        //     System.out.println(iterator.next());
-        // }
-
-        ArrayList<Integer> numbs = new ArrayList<Integer>();
-        numbs.add(1);
-        numbs.add(1);
-        numbs.add(1);
-        numbs.add(2);
-        numbs.add(2);
-        numbs.forEach(p -> System.out.println(p));
-        System.out.println("*********");
-        // Iterator<String> it = numbs.iterator();
-        // while (it.hasNext())
-        // System.out.println((it.next()));
-
         for (int i = 0; i < Polynomials.size() - 1; i++) {
-            System.out.println(Polynomials.get(i).degree + " next " + Polynomials.get(i + 1).degree);
-
-            // if (numbs.get(i) == numbs.get(i+1) || (i >= 1 && numbs.get(i) == numbs.get(i - 1)))
-            // {
-            //     int somme = numbs.get(i) + numbs.get(i+1);
-            //     numbs.set(i+1, somme * 10000);
-            //     numbs.remove(i);
-            // }
-
-            if (Polynomials.get(i) == Polynomials.get(i+1) || (i >= 1 && Polynomials.get(i) == Polynomials.get(i - 1)))
-            {
-                float sommeCoef = Polynomials.get(i).coefficient + Polynomials.get(i+1).coefficient;
-                PolynomialEquation pl = new PolynomialEquation(sommeCoef, Polynomials.get(i).degree);
-                Polynomials.set(i+1, pl);
+            if (Polynomials.get(i).getDegree() == Polynomials.get(i + 1).getDegree()) {
+                float sumCoef = Polynomials.get(i).getCoefficient() + Polynomials.get(i + 1).getCoefficient();
+                PolynomialEquation pl = new PolynomialEquation(sumCoef, Polynomials.get(i).getDegree());
+                Polynomials.set(i + 1, pl);
                 Polynomials.remove(i);
+                i--;
             }
         }
 
-        // numbs.forEach(p -> System.out.println(p));
+        Polynomials.forEach(p -> System.out.println(p));
+        System.out.println("****** Concat ****");
 
+        StringBuilder reduced = new StringBuilder();
+
+        Polynomials.forEach(p -> {
+            reduced.append(p.toString());
+        });
+
+      
+        maxDegree = Integer.parseInt(reduced.substring(reduced.lastIndexOf("^") + 1));
+
+        if (sumNumbers >= 0)
+            reduced.append("+" + Float.toString(sumNumbers) + " = 0");
+        else
+            reduced.append(Float.toString(sumNumbers) + " = 0");
+
+        System.out.println("Reduced form: " + reduced);
+        System.out.println("Polynomial degree: " + maxDegree);
+        // String reducedEqu = reduced.toString().replace(".0", "");
+
+        // System.out.println(reducedEqu);
+
+        Double delta = Polynomials.get(0).getCoefficient() * sumNumbers  * -4 + Math.sqrt(Polynomials.get(1).getCoefficient());
+
+        System.out.println("delta = " + delta);
+// ax2 + bx + c  ==> b2 – 4ac
+// bx + ax^2 + c
+        return " ";
     }
 
     public static void main(String[] args) {
@@ -138,26 +120,6 @@ public class Main {
                 throw new ComputorV1Exception("Equation BAD FORMAT\n");
             reducedForm(args[0]);
 
-            // // Pattern p =
-            // Pattern.compile("^(?!\\*)((\\+|-)?(?!$|\\+|-|=|\\*)((\\d+(\\.\\d+)?)?(\\*(?=[A-z]))?([A-z](\\^\\d+)?)?)(?=\\+|-|=|$))+");
-            // Pattern p =
-            // Pattern.compile("^((\\+|-)?((\\d+(\\.\\d+)?)?(\\*(?=[A-z]))?([A-z](\\^\\d+)?)?)(?=\\+|-|=|$))+");
-
-            // //
-            // System.out.println(("+8.12*X^0-6*X^1+0*X^2-5.6*X^3+12+12*X^10").matches("^((\\+|-)?((\\d+(\\.\\d+)?)?(\\*(?=[A-z]))?([A-z](\\^\\d+)?)?)(?=\\+|-|=|$))+"));
-            // // Matcher m = p.matcher("+8.12*X^0-6*X^1+0*X^2-5.6*X^3+12+12*X^10");
-            // Matcher m = p.matcher("+8*X^2+2*X^3");
-            // if (m.matches()) {
-            // // System.out.println("whole " + m.group(0));
-            // for (int i = 0; i < m.groupCount(); i++)
-            // System.out.println("first " + m.group(i));
-            // // // System.out.println("second " + m.group(2));
-            // }
-            // // while (m.find()) {
-            // // // Get the group matched using group() method
-            // // System.out.println(m.group());
-            // // }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -165,9 +127,9 @@ public class Main {
 }
 
 class PolynomialEquation {
-    float coefficient;
+    private float coefficient;
     // char variable;
-    int degree; // exponent
+    private int degree; // exponent
     // int constant;
 
     PolynomialEquation(float cf, int d) {
@@ -186,7 +148,12 @@ class PolynomialEquation {
 
     @Override
     public String toString() {
-        return this.coefficient + " ^ " + this.degree;
-    }
+        String coef;
+        if (coefficient >= 0)
+            coef = "+" + Float.toString(this.coefficient);
+        else
+            coef = Float.toString(this.coefficient);
 
+        return coef + "*X^" + this.degree;
+    }
 }
