39d1ec6b375536eae3075c87a6e77056682df2b4
Ayoub Bouras <aybouras@student.1337.ma>
2022-04-28 17:30:25 +0000

    solving

--- Main.java
+++ Main.java
@@ -9,9 +9,19 @@ public class Main {
 
     static String reducedForm(String input, char letter) {
 
-        for (int i = 0; i < input.length(); i++) {
-            if ((i == 0 && input.charAt(i) == letter)
-                    || (i > 0 && input.charAt(i) == letter && input.charAt(i - 1) != '*')) {
+// X^2
+
+// 4X^2
+// -X^2
+
+// 2*X
+
+        if (input.charAt(0) == letter)
+            input = ("1*").concat(Character.toString(letter)) + input.substring(1);
+        
+        
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == letter && input.charAt(i - 1) != '*') {
                 if (Character.isDigit(input.charAt(i - 1))) {
                     input = input.substring(0, i) + ("*").concat(Character.toString(letter)) + input.substring(i + 1);
                     i++;
@@ -20,14 +30,13 @@ public class Main {
                     i += 2;
                 }
             }
-            if ((i == input.length() - 1 && input.charAt(i) == letter)
-                    || (i + 1 != input.length() && input.charAt(i) == letter && input.charAt(i + 1) != '^')) {
+            if ((input.charAt(i) == letter && i == input.length() - 1)
+                    || (input.charAt(i) == letter && i + 1 != input.length() &&  input.charAt(i + 1) != '^')) {
                 input = input.substring(0, i) + Character.toString(letter).concat("^1") + input.substring(i + 1);
                 i += 2;
             }
         }
-        System.out.println("input = " + input);
-        // System.exit(0);
+        // System.out.println("input = " + input);
         String[] arr = input.split("=");
         String leftHandSide = arr[0];
         String rightHandSide = arr[1];
@@ -37,12 +46,12 @@ public class Main {
         if (Character.isDigit(rightHandSide.charAt(0)))
             rightHandSide = ("+").concat(rightHandSide);
 
-        // if (rightHandSide.matches("^\\d+"))
-        // rightHandSide = ("+").concat(rightHandSide);
-
         if (Character.isDigit(leftHandSide.charAt(0)))
             leftHandSide = ("+").concat(leftHandSide);
-        System.out.println("rightHandSide = " + rightHandSide);
+
+        System.out.println("\nrightHandSide = " + rightHandSide);
+        System.out.println("leftHandSide  = " + leftHandSide + "\n");
+    
         for (int i = 0; i < rightHandSide.length(); i++) {
             if (rightHandSide.charAt(i) == '+') {
                 rightHandSide = rightHandSide.substring(0, i) + "#-" + rightHandSide.substring(i + 1);
@@ -52,21 +61,24 @@ public class Main {
                 i++;
             }
         }
+    
         leftHandSide = leftHandSide.replace("+", "#+");
         leftHandSide = leftHandSide.replace("-", "#-");
 
         String fullEquation = leftHandSide.concat(rightHandSide);
-        System.out.println("newRightHandSide = " + rightHandSide);
-        System.out.println("full = " + fullEquation);
+        // System.out.println("newRightHandSide = " + rightHandSide);
+        // System.out.println("full = " + fullEquation);
 
         String[] splitString = fullEquation.split("#");
 
-        System.out.println("len = " + splitString.length);
+        // System.out.println("len = " + splitString.length);
 
         float sumNumbers = 0;
 
+        System.out.println("****** Extracting Terms ****");
+
         for (String str : splitString) {
-            System.out.println("===== " + str + " ======");
+            System.out.println(str );
             if (str.indexOf(letter) == -1) {
                 if (!str.isEmpty())
                     sumNumbers += Float.parseFloat(str);
@@ -80,9 +92,9 @@ public class Main {
             }
         }
 
-        System.out.println("Summmmm " + sumNumbers);
+        // System.out.println("Summmmm " + sumNumbers);
 
-        System.out.println("****** sorted ****");
+        System.out.println("\n****** Sort Terms By Degree ****\n");
 
         // Polynomials.forEach(p -> System.out.println(p));
         Collections.sort(Polynomials, new Comparator<PolynomialEquation>() {
@@ -94,7 +106,7 @@ public class Main {
 
         Polynomials.forEach(p -> System.out.println(p));
 
-        System.out.println("****** Sum ****");
+        System.out.println("\n****** Sum By Same Degree ****\n");
 
         for (int i = 0; i < Polynomials.size() - 1; i++) {
             if (Polynomials.get(i).getDegree() == Polynomials.get(i + 1).getDegree()) {
@@ -107,7 +119,7 @@ public class Main {
         }
 
         Polynomials.forEach(p -> System.out.println(p));
-        System.out.println("****** Concat ****");
+        System.out.println("\n****** Concat Terms ****\n");
 
         StringBuilder reduced = new StringBuilder();
 
@@ -115,14 +127,12 @@ public class Main {
             reduced.append(p.toString());
         });
 
-        maxDegree = Integer.parseInt(reduced.substring(reduced.lastIndexOf("^") + 1));
+                System.out.println("Reduced formmmmm: " + reduced);
 
-        // if (sumNumbers > 0)
-        // reduced.append("+" + Float.toString(sumNumbers) + " = 0");
-        // else if (sumNumbers == 0)
-        // reduced.append(" = 0");
-        // else
-        // reduced.append(Float.toString(sumNumbers) + " = 0");
+        System.out.println("educed.substring(reduced.lastIndexOf(^)" + reduced.substring(reduced.lastIndexOf("^")).toString());
+        // System.out.println("educed.substring(reduced.lastIndexOf(^) + 1" + reduced.substring(reduced.lastIndexOf("^") + 1).toString());
+
+        maxDegree = Integer.parseInt(reduced.substring(reduced.lastIndexOf("^") + 1));
 
         if (sumNumbers > 0)
             reduced.append("+" + (detectType.isInteger(sumNumbers) ? Integer.toString((int) sumNumbers)
@@ -133,17 +143,28 @@ public class Main {
             reduced.append((detectType.isInteger(sumNumbers) ? Integer.toString((int) sumNumbers)
                     : Float.toString(sumNumbers) + " = 0"));
 
-        System.out.println("Reduced form: " + reduced);
-        System.out.println("Polynomial degree: " + maxDegree);
-        String reducedEqu = reduced.toString().replace(".0", "");
-        // System.out.println("reddduced " + reducedEqu);
-        System.out.println("Reduced form: " + reducedEqu);
+        // System.out.println("Reduced form: " + reduced);
+        if (reduced.charAt(0) == '+')
+            reduced.replace(0, 1, "");
+        
+        System.out.print("Reduced form: ");
+        reduced.toString().chars().forEach(
+                c -> {
+                    if (c == '+' || c == '-' || c == '*')
+                        System.out.print(" " + Character.toString((char) c) + " ");
+                    else
+                        System.out.print(Character.toString((char) c));
+                });
+        System.out.print("\n");
+
+        System.out.println("Polynomial degree: " + maxDegree + "\n");
+       
         if (maxDegree > 2) {
             System.err.println("The polynomial degree is strictly greater than 2, I can't solve.");
             System.exit(1);
         }
         Float A;
-        if (reducedEqu.indexOf("^2") != -1) {
+        if (reduced.indexOf("^2") != -1) {
             if (Polynomials.size() == 2)
                 A = Polynomials.get(1).getCoefficient();
             else
@@ -151,20 +172,21 @@ public class Main {
         } else
             A = 0.0f;
 
-        Float B = (reducedEqu.indexOf("^1") != -1) ? Polynomials.get(0).getCoefficient() : 0.0f;
+        Float B = (reduced.indexOf("^1") != -1) ? Polynomials.get(0).getCoefficient() : 0.0f;
 
         System.out.println("A = " + A);
         System.out.println("B = " + B);
         System.out.println("C = " + sumNumbers);
 
+        System.out.println("    Where: Ax^2 + Bx + C = 0\n");
+        
         Double delta = (double) (B * B - 4 * A * sumNumbers);
+        
+        System.out.println("delta = B^2 - 4AC = " + delta + "\n");
 
-        System.out.println("delta = " + delta);
-        // ax2 + bx + c ==> b*b – 4ac
-        // bx + ax^2 + c
 
         Float res;
-        if (reducedEqu.indexOf("^2") == -1) {
+        if (reduced.indexOf("^2") == -1) {
             if (sumNumbers == 0)
                 res = 0.0f;
             else {
@@ -177,16 +199,18 @@ public class Main {
             if (delta > 0) {
                 System.out.println("Discriminant is strictly positive, the two solutions are: ");
 
+                
                 Double x1 = (-B + Math.sqrt(delta)) / (2 * A);
                 Double x2 = (-B - Math.sqrt(delta)) / (2 * A);
-
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
+                
+                System.out.println("x1 = -B + sqrt(Δ) / 2A = " + x1);
+                System.out.println("x2 = -B - sqrt(Δ) / 2A = " + x2);
             }
 
             if (delta == 0) {
                 System.out.println("Discriminant is Equal to Zero ");
-                System.out.println("the quadratic equation has two equal real roots.: ");
+                System.out.println("the quadratic equation has two equal real roots: ");
+                System.out.println("x1 = x2 = -B / 2A");
 
                 System.out.println("x1 = x2 = " + (-B / (2 * A)));
             }
@@ -222,9 +246,6 @@ public class Main {
             final char tempLetter = letter;
             long countVariables = args[0].chars().filter(ch -> ch == tempLetter).count();
             long countAlphas = args[0].chars().filter(ch -> Character.isLetter(ch)).count();
-
-            System.out.println("countVariables = " + countVariables);
-            System.out.println("countAlphas = " + countAlphas);
 
             if (countVariables != countAlphas)
                 throw new ComputorV1Exception("Equation BAD FORMAT: There Are Multiple Diffirent Variables\n");
@@ -265,7 +286,6 @@ class PolynomialEquation {
                     : Float.toString(this.coefficient));
         else
             coef = Float.toString(this.coefficient);
-        // System.out.println("coef = " + coef);
         if (getCoefficient() == 0)
             return "";
         return coef + "*" + Character.toString(variable) + "^" + this.degree;
